57df2de8862f370cab0def18a2c3263a2f290807
Arthur <Arthur@DESKTOP-RLI9PUB>
2026-05-18 22:42:58 +0900

    Documentação de cada página.

--- core/views.py
+++ core/views.py
@@ -1,13 +1,18 @@
+from django.http import HttpRequest
 from django.shortcuts import render
 
-def autenticacao(request):
+def autenticacao(request: HttpRequest):
+    """Página de login e cadastro."""
     return render(request, 'autenticacao.html')
 
-def detalhes(request):
+def detalhes(request: HttpRequest):
+    """Detalhes sobre a pergunta (data/autor/...). Listagem e adição de respostas."""
     return render(request, 'detalhes.html')
 
-def listagem(request):
+def listagem(request: HttpRequest):
+    """Filtragem e listagem de perguntas."""
     return render(request, 'listagem.html')
 
-def perguntar(request):
+def perguntar(request: HttpRequest):
+    """Criação de nova pergunta."""
     return render(request, 'perguntar.html')
