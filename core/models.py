253c2331ce56a4b0f79a2b8efe9aa55c3e2e50bb
Arthur <Arthur@DESKTOP-RLI9PUB>
2026-05-19 01:52:21 +0900

    Campos obrigatórios para a criação de pergunta e cadastro.

--- core/models.py
+++ core/models.py
@@ -1,4 +1,5 @@
 from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 from django.db import models
 
 class Perfil(models.Model):
@@ -9,7 +10,7 @@ class Perfil(models.Model):
         ('ti', 'Tecnologia da Informação'),
     ]
     departamento = models.CharField(max_length=3, choices=DEPARTAMENTO_CHOICES)
-    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil')
+    usuario = models.OneToOneField(User, on_delete=models.CASCADE, related_name='perfil', validators=[MinLengthValidator(6)])
  
     def __str__(self):
         return f"{self.usuario.username} – {self.departamento}"
@@ -18,9 +19,9 @@ class Pergunta(models.Model):
     STATUS_CHOICES = [('aberta', 'Aberta'), ('fechada', 'Fechada'), ('respondida', 'Respondida')]
     autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='solicitacoes')
     criado_em = models.DateTimeField(auto_now_add=True)
-    pergunta = models.TextField()
+    pergunta = models.TextField(validators=[MinLengthValidator(10)])
     status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='aberta')
-    titulo = models.CharField(max_length=200)
+    titulo = models.CharField(max_length=200, validators=[MinLengthValidator(10)])
 
     class Meta:
         ordering = ['-criado_em']
