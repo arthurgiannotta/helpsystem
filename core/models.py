73c8950a219347a6a2b4918988311c542b5a6b6b
Arthur <Arthur@DESKTOP-RLI9PUB>
2026-05-18 23:54:38 +0900

    Estrutura do banco de dados de perguntas.

--- core/models.py
+++ core/models.py
@@ -1,3 +1,16 @@
+from django.contrib.auth.models import User
 from django.db import models
 
-# Create your models here.
+class Pergunta(models.Model):
+    STATUS_CHOICES = [('aberta', 'Aberta'), ('fechada', 'Fechada'), ('respondida', 'Respondida')]
+    autor = models.ForeignKey(User, on_delete=models.CASCADE, related_name='solicitacoes')
+    criado_em = models.DateTimeField(auto_now_add=True)
+    pergunta = models.TextField()
+    status = models.CharField(max_length=20, choices=STATUS_CHOICES, default='aberta')
+    titulo = models.CharField(max_length=200)
+
+    class Meta:
+        ordering = ['-criado_em']
+
+    def __str__(self):
+        return self.titulo
